9a643c4c5644d437150bd2f17162f41a76d2657a
Samuel <samuelggomez710@gmail.com>
2026-02-03 17:49:11 -0600

    Removed server startup from app.js

--- server/app.js
+++ server/app.js
@@ -75,10 +75,5 @@ app.use((req, res) => {
     res.status(200).send('ok');
 });
 
-// Server startup
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = app;
